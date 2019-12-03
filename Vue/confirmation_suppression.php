eaac741cffb82afbcabe150a58e8dcee621d2f60
ReineDeLaNight <germain40k@hotmail.fr>
2019-12-03 20:58:36 +0100

    je fais des betises pardon

--- Vue/confirmation_suppression.php
+++ Vue/confirmation_suppression.php
@@ -7,10 +7,10 @@
 <body>
     <h2>Nous serions désolés de vous voir partir</h2>
     <h2>Etes-vous certain de vouloir supprimer votre compte ?</h2>
-    <form action="../Controleur/suppression_compte.php">
+    <form action="./suppression_compte.php">
         <button type="submit"> Oui je souhaite supprimer mon compte </button>
     </form>
-    <form action="../Controleur/profil.php">
+    <form action="./profil.php">
         <button type="submit"> Non, OKAZOU c'est trop bien ! </button>
     </form>
 </body>
