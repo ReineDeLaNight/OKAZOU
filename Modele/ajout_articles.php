e5329994b7f8128aaef001bad5f43b2da5d6ef86
LevWasTaken <arthur22032000@gmail.com>
2019-12-11 22:46:38 +0100

    Ajout articles operationnel + dossier image

---
+++ Modele/ajout_articles.php
@@ -0,0 +1,90 @@
+<?php
+
+function ajout_articles() {
+    $description = $_GET['description'];
+    $prix = $_GET['prix'];
+    $keyword = $_GET['keyword'];
+    $couleur = $_GET['couleur'];
+    $etat = $_GET['etat'];
+    $photo1 = $_GET['photo1'];
+    $photo2 = $_GET['photo2'];
+    $photo3 = $_GET['photo3'];
+    $categorie = $_GET['categorie'];
+    $taille = $_GET['taille'];
+    $site = $_GET['site'];
+    $marque = $_GET['marque'];
+    
+    $bdd = new PDO("mysql:host=localhost;dbname=okazou","root","");
+    if(substr($categorie,0, 1) == "h") {
+        $idPere = 1;
+        $majCategorie = substr($categorie, 1);
+    } else if (substr($categorie,0, 1) == "f") {
+        $idPere = 2;
+        $majCategorie = substr($categorie, 1); 
+    } else if (substr($categorie,0, 1) == "e") {
+        $idPere = 3;
+        $majCategorie = substr($categorie, 1); 
+    }
+    $req1 = $bdd->prepare("SELECT id FROM categorie WHERE nom_categorie LIKE :majCategorie AND pere LIKE :idPere");
+    $req1 ->bindParam(':idPere', $idPere, PDO::PARAM_INT);
+    $req1 ->bindParam(':majCategorie', $majCategorie, PDO::PARAM_STR);
+    $req1 -> execute();
+    $idCategorie = $req1 -> fetch();
+    $idCategorie = $idCategorie[0];
+    echo($idCategorie."<br>");
+
+    $req2 = $bdd -> prepare("SELECT id FROM site WHERE nom LIKE :site");
+    $req2 ->bindParam(':site', $site, PDO::PARAM_STR);
+    $req2 -> execute();
+    $idSite = $req2 -> fetch();
+    $idSite = $idSite[0];
+    echo($idSite."<br>");
+    
+    if(substr($taille,0, 1) == "h") {
+        $categorieTaille = "hauts";
+        $majTaille = substr($taille, 1);
+    } else if (substr($taille,0, 1) == "c") {
+        $categorieTaille = "chaussure";
+        $majTaille = substr($taille, 1); 
+
+    } else if (substr($taille,0, 1) == "p") {
+        $categorieTaille = "pantalons";
+        $majTaille = substr($taille, 1); 
+    } else if (substr($taille,0, 1) == "ch") {
+        $categorieTaille = "chapeau";
+        $majTaille = substr($taille, 1); 
+    }
+    $req3 = $bdd -> prepare("SELECT id FROM taille WHERE taille LIKE :majTaille AND categorie LIKE :categorieTaille");
+    $req3 ->bindParam(':categorieTaille', $categorieTaille, PDO::PARAM_STR);
+    $req3 ->bindParam(':majTaille', $majTaille, PDO::PARAM_STR);
+    $req3 -> execute();
+    $idTaille = $req3 -> fetch();
+    $idTaille = $idTaille[0];
+    echo($idTaille."<br>");
+
+    $req4 = $bdd -> prepare("SELECT id FROM marque WHERE marque LIKE :marque");
+    $req4 ->bindParam(':marque', $marque, PDO::PARAM_STR);
+    $req4 -> execute();
+    $idMarque = $req4 -> fetch();
+    $idMarque = $idMarque[0];
+    echo($idMarque);
+
+    $reqAjout = $bdd -> prepare("INSERT INTO article(description, prix, keyword, couleur, etat, photo1, photo2, photo3, taille, categorie, site, marque) VALUES (:description, :prix, :keyword, :couleur, :etat, :photo1, :photo2, :photo3, :idTaille, :idCategorie, :idSite, :idMarque)");
+    $reqAjout ->bindParam(':description', $description,  PDO::PARAM_STR);
+    $reqAjout ->bindParam(':prix', $prix,  PDO::PARAM_INT);
+    $reqAjout ->bindParam(':couleur', $couleur,  PDO::PARAM_STR);
+    $reqAjout ->bindParam(':etat', $etat,  PDO::PARAM_STR);
+    $reqAjout ->bindParam(':photo1', $photo1,  PDO::PARAM_STR);
+    $reqAjout ->bindParam(':photo2', $photo2,  PDO::PARAM_STR);
+    $reqAjout ->bindParam(':photo3', $photo3,  PDO::PARAM_STR);
+    $reqAjout ->bindParam(':keyword', $keyword,  PDO::PARAM_STR);
+    $reqAjout ->bindParam(':idTaille', $idTaille,  PDO::PARAM_INT);
+    $reqAjout ->bindParam(':idCategorie', $idCategorie,  PDO::PARAM_INT);
+    $reqAjout ->bindParam(':idSite', $idSite,  PDO::PARAM_INT);
+    $reqAjout ->bindParam(':idMarque', $idMarque,  PDO::PARAM_INT);
+    $reqAjout -> execute();
+    
+    
+    
+}
+?>
---
+++ Vue/ajout_articles.php
@@ -0,0 +1,95 @@
+<?php
+  
+?>
+<html>
+    <head>
+        <title> Ajouter des articles </title>
+    </head>
+
+    <body>
+		<h1>Ajoutez un article</h1>
+       <form action="ajout_articles.php" method="get" >
+     <p> Ajoutez une description (45  caracteres max) </p> <input type="text" name="description"/> 
+     <p> Prix (en euros) </p> <input type="number" name="prix" step="0.01"/> 
+     <p> Ajoutez des mots clés pour le référencement </p> <input type="text" name="keyword"/>
+     <p> Couleur</p> <select name="couleur">
+        <option value="noir" >Noir</option>
+        <option value="gris" >Gris</option>
+        <option value="blanc" >Blanc</option>
+        <option value="beige" >Beige</option>
+        <option value="orange" >Orange</option>
+        <option value="rouge" >Rouge</option>
+        <option value="rose" >Rose</option>
+        <option value="violet" >Violet</option>
+        <option value="bleuclair" >Bleu Clair</option>
+        <option value="bleu" >Bleu</option>
+        <option value="marine" >Marine</option>
+        <option value="turquoise" >Turquoise</option>
+        <option value="vert" >Vert</option>
+        <option value="vertfonce" >Vert foncé</option>
+        <option value="marron" >Marron</option>
+        <option value="jaune" >Jaune</option>
+      </select>
+     <p>Etat :</p> <select name="etat">
+        <option value="neufEtiquette"> Neuf, avec étiquette </option>
+        <option value="neuf"> Neuf </option>
+        <option value="tresBonEtat"> Très bon état </option>
+        <option value="bonEtat"> Bon état </option>
+        <option value="satisfaisant"> Satisfaisant </option>
+     </select>
+     <p> Ajoutez de 1 a 3 photos :</p> <input type = "file" name="photo1" accept=".png"> <input type = "file" name="photo2" accept=".png"> <input type = "file" name="photo3" >
+     <p>Catégorie :</p> <select name="categorie">
+        <optgroup label ="Hommes">
+        <option value="hVestesManteauxBlousons"> Vestes Manteaux & Blousons </option>
+        </optgroup>
+        <optgroup label ="Femmes">
+        <option value="fVestesManteauxBlousons"> Vestes Manteaux & Blousons </option>
+        </optgroup>
+        <optgroup label ="Enfants">
+        <option value="eVestesManteauxBlousons"> Vestes Manteaux & Blousons </option>
+        </optgroup>
+     </select>
+     <p>Taille</p> <select name="taille">
+        <optgroup label ="Hauts">
+        <option value="hxs"> XS </option>
+        <option value="hs"> S </option>
+        <option value="hm"> M </option>
+        <option value="hl"> L </option>
+        <option value="hxl"> XL </option>
+        </optgroup>
+        <optgroup label ="Chaussures">
+        <option value="c38"> 38 </option>
+        <option value="c38.5"> 38.5 </option>
+        <option value="c39"> 39 </option>
+        <option value="c39.5"> 39.5 </option>
+        <option value="c40"> 40 </option>
+        <option value="c40.5"> 40.5 </option>
+        <option value="c41"> 41 </option>
+        <option value="c41.5"> 41.5 </option>
+        <option value="c42"> 42 </option>
+        <option value="c42.5"> 42.5 </option>
+        <option value="c43"> 43 </option>
+        <option value="c43.5"> 43.5 </option>
+        <option value="c44"> 44 </option>
+        <option value="c44.5"> 44.5 </option>
+        <option value="c45"> 45 </option>
+        <optgroup>
+        <optgroup label ="Pantalons">
+        <optgroup label ="Hommes">
+        <option value="flemme"> MEURS </option>
+        <optgroup>
+        <optgroup label ="Femmes">
+        <option value="de ouf"> MEURS </option>
+        <optgroup>
+        <optgroup>
+     </select>
+     <p>Site</p> <select name="site">
+     <option value="vinted"> Vinted </option>
+     <option value="vestiairecollective"> Vestiaire Collective </option>
+     </select>
+     <p>Marque</p> <select name="marque">
+     <option value="don dada"> Don Dada</option>
+     </select>
+    <p> <input type='submit' value=ok name='valide' ><p> </form>
+    </body>
+</html>
